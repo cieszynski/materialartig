defaef79ba70d72f667984e24c26be9fe8b99201
cieszynski <admin@cieszynski.de>
2024-06-02 14:09:31 +0200

    initial deploy

---
+++ public/sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (event) => {
+
+});
+
+self.addEventListener("activate", (event) => {
+
+});
+
+self.addEventListener("fetch", (event) => {
+
+});
+
+self.addEventListener("sync", (event) => {});
+
+self.addEventListener("push", (event) => {});
